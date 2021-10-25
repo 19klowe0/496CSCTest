2d99e957e1958932e69d1421e4e8aee38d78c7c9
Kira Lowe <kiralowe98@gmail.com>
2021-10-25 17:53:16 -0400

    blog post are right!
    Working on Pokemon API now

---
+++ src/template/post.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+import { graphql } from "gatsby"
+import PropTypes from "prop-types"
+class Post extends Component {
+  render() {
+    const post = this.props.data.wordpressPost
+    return (
+      <>
+        <h1>{post.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      </>
+    )
+  }
+}
+const data = JSON.stringify({
+    query: `query($id: String!) {
+		allWpPost {
+			title
+			content
+		}
+	}`,
+    variables: `{
+        "id": "${id}"
+      }`,
+  });
