bfe2a4be8f3cc9b75d02ef434c44b41ce8982323
Kira Lowe <kiralowe98@gmail.com>
2021-11-15 19:18:46 -0500

    Framer-motion branch
    this branch has some framer motion implemented in the code , but for some reason will not work

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-plugin-styled-components",
     `gatsby-transformer-sharp`,
+    'gatsby-plugin-dark-mode',
     {
       resolve: "gatsby-source-filesystem",
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,38 @@
 import * as React from 'react'
-
-
+import Nav from '../components/nav'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
-
+import {motion} from "framer-motion"
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
+      <div>
+      <motion.div inital ="hidden" animate= "visible" variants= {{
+        hidden:{
+          scale: .8, 
+          opacity:0
+        }, 
+        visible:{
+          scale: 1, 
+          opacity:1, 
+          transition: {
+            delay:4
+          }
+        }
+      }}>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      </motion.div>
       <StaticImage
                alt="frog picture that I liked"
                src="../images/frog.jpg"
       />
+      <dark-button>
+        Toggle Dark Mode
+      </dark-button>
+      </div>
     </Layout>
   )
 }
+
 
 export default IndexPage
